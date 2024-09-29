35c3b2611bb473b05423faf7f28dd37762736615
melonQT <129608612+melonQT@users.noreply.github.com>
2024-09-30 01:07:39 +0530

    Delete database directory

--- database/__init__.py
+++
@@ -1,9 +0,0 @@
-from config import Config
-from motor.motor_asyncio import AsyncIOMotorClient
-import dns.resolver
-dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
-dns.resolver.default_resolver.nameservers=['8.8.8.8']
-
-
-Database = AsyncIOMotorClient(Config.DATABASE_URL)
-db = Database.Bot
